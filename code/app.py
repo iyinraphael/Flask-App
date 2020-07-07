b54f536603bee0b10ddbc668e3818aabfb70a01a
iyinraphael <iyinraphael@gmail.com>
2020-07-07 11:30:41 -0700

    put and delete

--- code/app.py
+++ code/app.py
@@ -20,7 +20,7 @@ class Item(Resource):
         help="This field cannot be left blank!"
     )
 
-    @jwt_required
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if them else 404
@@ -29,7 +29,7 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item
@@ -38,6 +38,17 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class ItemList(Resource):
     def get(self):

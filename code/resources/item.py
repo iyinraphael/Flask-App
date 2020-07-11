0eb91d4d281620274e4c709ea6ad53e7f03602fe
iyinraphael <iyinraphael@gmail.com>
2020-07-10 23:31:06 -0700

    code refractor

---
+++ code/resources/item.py
@@ -0,0 +1,82 @@
+from flask import Flask, request
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required, current_identity
+from models.item import ItemModel
+
+import sqlite3
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': 'item not found'}, 404
+
+    def post(self, name):
+        # checks if item exists
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400 # User error
+
+        data = Item.parser.parse_args()
+
+        item = ItemModel(name, data['price'])
+
+        try:
+            item.insert()
+        except:
+            return {"message": "An error occured inserting the item"}, 500 # Internal server error
+
+        return item, 201
+
+    def delete(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name, data['price'])
+
+        if item is None:
+            try:
+                updated_item.insert()
+            except:
+                return {"message": "An error occured inserting the item"}, 500
+        else:
+            try:
+                updated_item.update()
+            except:
+                return {"message": "An error occured inserting the item"}, 500
+        return updated_item.json()
+
+class ItemList(Resource):
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
+        return{'items': items}
